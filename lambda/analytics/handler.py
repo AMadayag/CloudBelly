bdee33dabc04267b51c73ff88c9ef326b76c5adf
AMadayag <hiding303@gmail.com>
2026-03-16 20:52:27 +1100

    added from adn to for date

--- lambda/analytics/handler.py
+++ lambda/analytics/handler.py
@@ -14,17 +14,26 @@ def lambda_handler(event, context):
 def get_price_trend(event):
     params = event.get("queryStringParameters")
     location = params.get("suburb")
-    startDate = params.get("startDate")
+    startDate = params.get("from")
+    endDate = params.get("to")
 
     try:
-    if date:
+    if startDate and endDate:
         response = table.query(
-            KeyConditionExpression=Key('location').eq(location) & Key('date').gte(date)
+            KeyConditionExpression=Key('location').eq(location) & Key('date').between(startDate, endDate)
+        )
+    elif startDate:
+        response = table.query(
+            KeyConditionExpression=Key('location').eq(location) & Key('date').gte(startDate)
+        )
+    elif endDate:
+        response = table.query(
+            KeyConditionExpression=Key('location').eq(location) & Key('date').lte(endDate)
         )
     else:
         response = table.query(
             KeyConditionExpression=Key('location').eq(location)
-            )
+        )
 
     items = response.get('Items', [])
 
